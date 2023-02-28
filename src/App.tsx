e0cec8933976d179d5ae3d1825e38891417cff7d
Matt Moore <mattmoore@pop-os.localdomain>
2023-02-28 16:20:14 -0500

    moved components and models to own files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import EmailSigPreview from './EmailSigPreview';
-import EmailSigSettings from './EmailSigSettings';
+import SignaturePreview from './Components/SignaturePreview';
+import SignatureSettings from './Components/SignatureSettings';
 
 function App(): JSX.Element {
   return (
@@ -15,13 +15,13 @@ function App(): JSX.Element {
           id="signatureCreationOptions"
           className="flex flex-col md:h-screen md:w-1/4 bg-gradient-to-br from-blue-500/70 to-blue-400/70"
         >
-          <EmailSigSettings />
+          <SignatureSettings />
         </div>
         <div
           id="signaturePreview"
           className="flex flex-col items-center h-screen md:w-3/4 bg-gradient-to-br from-neutral-200 to-gray-100 py-2 px-6"
         >
-          <EmailSigPreview />
+          <SignaturePreview />
         </div>
       </div>
     </>
