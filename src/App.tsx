e0b3ae3a915eaa4631d540f5d8920df02d16afcb
Matt Moore <mattmoore@pop-os.localdomain>
2023-03-03 17:46:29 -0500

    Added radix and react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import { RecoilRoot } from 'recoil';
-import SignaturePreview from './Components/SignaturePreview';
-import SignatureSettings from './Components/SignatureSettings';
+import SignatureSettingsMenuBar from './components/SignatureSettingsMenuBar';
+import SignaturePreview from './components/SignaturePreview';
+import SignatureSettings from './components/SignatureSettings';
 
 function App(): JSX.Element {
   return (
@@ -10,20 +11,30 @@ function App(): JSX.Element {
       <>
         <div
           id="appWrapper"
-          className="flex flex-col md:flex-row font-quicksand text-gray-600"
+          className="flex flex-col font-quicksand text-gray-800"
         >
-          {/* Create EmailSignatureApp and lift state EmailSigSettings state up to it} */}
           <div
-            id="signatureCreationOptions"
-            className="flex flex-col md:h-screen md:w-1/4 bg-gradient-to-br from-blue-500/70 to-blue-400/70"
+            id="signatureSettingsMenu"
+            className="flex flex-row justify-center"
           >
-            <SignatureSettings />
+            <SignatureSettingsMenuBar />
           </div>
           <div
-            id="signaturePreview"
-            className="flex flex-col items-center h-screen md:w-3/4 bg-gradient-to-br from-neutral-200 to-gray-100 py-2 px-6"
+            id="signatureDetailsAndPreviewWrapper"
+            className="flex flex-col md:flex-row"
           >
-            <SignaturePreview />
+            <div
+              id="signatureCreationOptions"
+              className="flex flex-col md:h-screen md:w-1/4 bg-gradient-to-br from-blue-500/70 to-blue-400/70"
+            >
+              <SignatureSettings />
+            </div>
+            <div
+              id="signaturePreview"
+              className="flex flex-col items-center h-screen md:w-3/4 bg-gradient-to-br from-neutral-200 to-gray-100 py-2 px-6"
+            >
+              <SignaturePreview />
+            </div>
           </div>
         </div>
       </>
